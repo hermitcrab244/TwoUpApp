4fd40aa9d038fe19130cf085f4a5116bc345d806
Michael Hermann <mhermann244@outlook.com>
2023-05-22 13:27:45 +0930

    Testing commit

--- src/app/modules/main/components/game/game.component.spec.ts
+++ src/app/modules/main/components/game/game.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MaterialModule } from 'src/app/modules/shared/material.module';
 import { GameComponent } from './game.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('GameComponent', () => {
   let component: GameComponent;
@@ -8,16 +9,60 @@ describe('GameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameComponent ]
-    })
-    .compileComponents();
+      imports: [MaterialModule, BrowserAnimationsModule],
+      declarations: [GameComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update game message and enable/disable buttons when player selects a choice', () => {
+    component.playerSelected('Heads');
+    fixture.detectChanges();
+
+    expect(component.playerChoice).toEqual('Heads');
+    expect(component.gameMessage).toEqual('You guessed Heads');
+    expect(component.tossDisabled).toBeFalse();
+    expect(component.selectDisabled).toBeTrue();
+  });
+
+  it('should perform coin toss, update game message and show coin faces accordingly', () => {
+    spyOn(component, 'flipAnimation');
+    const expectedResults = 'Heads';
+    const expectedOutcome = 'win';
+
+    component.playerChoice = 'Heads';
+    component.playRound();
+    fixture.detectChanges();
+
+    expect(component.flipAnimation).toHaveBeenCalled();
+    expect(component.coin1).toContain('heads');
+    expect(component.coin2).toContain('heads');
+
+    // Simulate the delay for the coin flip animation
+    setTimeout(() => {
+      expect(component.gameMessage).toContain(expectedResults);
+      expect(component.sendResults).toHaveBeenCalledWith(
+        expectedResults,
+        expectedOutcome
+      );
+    }, 2000);
+  });
+
+  it('should generate correct coin faces for the coin toss', () => {
+    spyOn(Math, 'random').and.returnValues(0.3, 0.7); // Mock Math.random() to return fixed values
+
+    component.playerChoice = 'Heads';
+    component.playRound();
+    fixture.detectChanges();
+
+    expect(component.coin1).toContain('heads');
+    expect(component.coin2).toContain('tails');
   });
 });
